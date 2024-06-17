9ba21ad230669b25a473e21ce4c1483a1753e50c
blairtchalla821@gmail.com <blairtchalla821@gmail.com>
2024-06-17 19:05:23 +0300

    database connection

--- frontend/School Events/src/pages/UpComingEvents.jsx
+++ frontend/School Events/src/pages/UpComingEvents.jsx
@@ -7,8 +7,25 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
 import PageNavigation from "../components/PageNavigation";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "./utils";
 
 function UpComingEvents() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/events`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setEvents(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <Container fluid style={{ width: "18rem", marginTop: "10px" }}>
       <PageNavigation />
@@ -23,110 +40,31 @@ function UpComingEvents() {
           Upcoming Events
         </h1>
       </>
-      <Row>
-        <Col>
-          <Card
-            style={{ width: "18rem", marginTop: "10px", objectFit: "fill" }}
-          >
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Navbar className="bg-body-tertiary">
-                <Container>
-                  <Navbar.Brand href="/"></Navbar.Brand>
-                  <Nav.Item>
-                    <NavLink to="/book_Event">
-                      <Button variant="dark">Book Event</Button>
-                    </NavLink>
-                  </Nav.Item>
-                </Container>
-              </Navbar>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Card
-            style={{ width: "18rem", marginTop: "10px", objectFit: "fill" }}
-          >
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Navbar className="bg-body-tertiary">
-                <Container>
-                  <Navbar.Brand href="/"></Navbar.Brand>
-                  <Nav.Item>
-                    <NavLink to="/book_Event">
-                      <Button variant="dark">Book Event</Button>
-                    </NavLink>
-                  </Nav.Item>
-                </Container>
-              </Navbar>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Card
-            style={{ width: "18rem", marginTop: "10px", objectFit: "fill" }}
-          >
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Navbar className="bg-body-tertiary">
-                <Container>
-                  <Navbar.Brand href="/"></Navbar.Brand>
-                  <Nav.Item>
-                    <NavLink to="/book_Event">
-                      <Button variant="dark">Book Event</Button>
-                    </NavLink>
-                  </Nav.Item>
-                </Container>
-              </Navbar>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Card
-            style={{ width: "18rem", marginTop: "10px", objectFit: "fill" }}
-          >
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Navbar className="bg-body-tertiary">
-                <Container>
-                  <Navbar.Brand href="/"></Navbar.Brand>
-                  <Nav.Item>
-                    <NavLink to="/book_Event">
-                      <Button variant="dark">Book Event</Button>
-                    </NavLink>
-                  </Nav.Item>
-                </Container>
-              </Navbar>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+      {events.map((event) => (
+        <Row key={event.id}>
+          <Col>
+            <Card
+              style={{ width: "18rem", marginTop: "10px", objectFit: "fill" }}
+            >
+              <Card.Img variant="top" src={event.image} />{" "}
+              <Card.Body>
+                <Card.Title>{event.title}</Card.Title>{" "}
+                <Card.Text>{event.description}</Card.Text>{" "}
+                <Navbar className="bg-body-tertiary">
+                  <Container>
+                    <Navbar.Brand href="/book_Event"></Navbar.Brand>
+                    <Nav.Item>
+                      <NavLink to="/book_Event">
+                        <Button variant="dark">Book Event</Button>
+                      </NavLink>
+                    </Nav.Item>
+                  </Container>
+                </Navbar>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      ))}
     </Container>
   );
 }
